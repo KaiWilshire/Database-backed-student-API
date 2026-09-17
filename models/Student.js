0ec0cfa4ac15c91f7c5d2c98f2083d53de3dc1ad
Zwe <2310040006@students.stamford.edu>
2026-09-17 10:01:35 +0700

    first commit

---
+++ models/Student.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const studentSchema = new mongoose.Schema({
+ name: {
+    type: String,
+    required: true
+ },
+ major: {
+    type: String,
+    required: true
+ },
+ score: {
+    type: Number,
+    min: 0,
+    max: 100,
+    required: true
+ }
+});
+module.exports = mongoose.model("Student", studentSchema);
